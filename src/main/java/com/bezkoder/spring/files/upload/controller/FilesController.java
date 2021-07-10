3351e305de8b64c9ae8b2914c1cced28c760cd40
CoolyDev <cephaszoubga@gmail.com>
2021-07-10 06:54:19 +0100

    ok

--- src/main/java/com/bezkoder/spring/files/upload/controller/FilesController.java
+++ src/main/java/com/bezkoder/spring/files/upload/controller/FilesController.java
@@ -25,43 +25,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @CrossOrigin("*")
 public class FilesController {
 
- /* @Autowired
-  FilesStorageService storageService;
-
-  @PostMapping("/upload")
-  public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
-    String message = "";
-    try {
-      storageService.save(file);
-
-      message = "Uploaded the file successfully: " + file.getOriginalFilename();
-      return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-    } catch (Exception e) {
-      message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-    }
-  }
-
-  @GetMapping("/files")
-  public ResponseEntity<List<FileInfo>> getListFiles() {
-    List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
-      String filename = path.getFileName().toString();
-      String url = MvcUriComponentsBuilder
-          .fromMethodName(FilesController.class, "getFile", path.getFileName().toString()).build().toString();
-
-      return new FileInfo(filename, url);
-    }).collect(Collectors.toList());
-
-    return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
-  }
-
-  @GetMapping("/files/{filename:.+}")
-  public ResponseEntity<Resource> getFile(@PathVariable String filename) {
-    Resource file = storageService.load(filename);
-    return ResponseEntity.ok()
-        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-  }*/
-
    @Autowired
    private FileStorageService storageService;
 
@@ -73,22 +36,11 @@ public class FilesController {
 
    @PostMapping("/upload")
    public Item uploadFile(@RequestParam("file") MultipartFile file) {
-     String message = "";
      try {
          return storageService.store(file);
-/*
-       Item mP=new Item();
-       mP.setName(file.getOriginalFilename());
-       mP.setData(file.getBytes());
-       mP.setType(file.getContentType());*/
-
-      // itemRepository.save(mP);
-
-
-      // return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
      } catch (Exception e) {
+         System.out.println(e);
       return null;
-       // return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
      }
    }
 
